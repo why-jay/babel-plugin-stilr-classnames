5f0d7116fab21e74fed6051f28a00f2b9393514b
YJ Yang <chcokr@gmail.com>
2015-09-21 23:09:18 -0400

    Adjust tests for 0.1.0

--- test/general/expectedHeader.js
+++ test/general/expectedHeader.js
@@ -1,3 +1,1 @@
-var stylesheet = "._1A3OE5{-webkit-box-align:center;-webkit-align-items:center;-ms-flex-align:center;align-items:center;}";var stilrStyleElem = document.getElementById('babel_stilrcx_style');if (!stilrStyleElem) {
-  stilrStyleElem = document.createElement('style');stilrStyleElem.id = 'babel_stilrcx_style';document.head.appendChild(stilrStyleElem);
-}stilrStyleElem.textContent = stylesheet;module.exports.stilrStylesheet = stylesheet;
+var stilrStyleElem = document.getElementById('babel_stilrcx_style');if (!stilrStyleElem) {  stilrStyleElem = document.createElement('style');stilrStyleElem.id = 'babel_stilrcx_style';document.head.appendChild(stilrStyleElem);}stilrStyleElem.textContent = "._1A3OE5{-webkit-box-align:center;-webkit-align-items:center;-ms-flex-align:center;align-items:center;}";var stylesheet = '_1A3OE5';
--- test/test.js
+++ test/test.js
@@ -10,6 +10,8 @@ describe('Plugin', function () {
     testFilePaths.forEach(function (filePath) {
 
       it(path.basename(filePath, '.js'), function () {
+        process.env.NODE_ENV = 'development';
+        // so that global.stilrStylesheet isn't polluted
 
         babel.transformFileSync(
           path.join(__dirname, 'evaluatePath', filePath),
@@ -24,23 +26,61 @@ describe('Plugin', function () {
 
   });
 
-  var transformResult =
-    babel.transformFileSync(path.join(__dirname, 'general', 'targetFile.js'), {
-      plugins: ['../dist/index']
+  describe(', when process.env.NODE_ENV === "production",',
+    function () {
+      process.env.NODE_ENV = 'development';
+
+      var transformResult =
+        babel.transformFileSync(
+          path.join(__dirname, 'general', 'targetFile.js'),
+          {
+            plugins: ['../dist/index']
+          }
+        );
+
+      it('writes proper header at beginning of program', function () {
+        var expectedHeader =
+          fs.readFileSync(path.join(__dirname, 'general', 'expectedHeader.js'))
+            .toString()
+            .replace(/\n/g, '');
+        assert.notDeepEqual(
+          transformResult.code.replace(/\n/g, '').indexOf(expectedHeader),
+          -1);
+      });
+
+      it('replaces the function call with the return value', function () {
+        assert.notDeepEqual(
+          transformResult.code.indexOf("var stylesheet = '_1A3OE5'"),
+          -1);
+      });
     });
 
-  it('writes proper header at beginning of program', function () {
-    var expectedHeader =
-      fs.readFileSync(path.join(__dirname, 'general', 'expectedHeader.js'))
-        .toString()
-        .replace(/\n/g, '');
-    assert(
-      transformResult.code.replace(/\n/g, '').indexOf(expectedHeader) !== -1);
+  describe(', when process.env.NODE_ENV === "production",',
+    function () {
+      process.env.NODE_ENV = 'production';
 
-  });
+      babel.transformFileSync(
+        path.join(__dirname, 'general', 'targetFile.js'),
+        {
+          plugins: ['../dist/index']
+        }
+      );
 
-  it('replaces the function call with the return value', function () {
-    assert(transformResult.code.indexOf("var stylesheet = '_1A3OE5'") !== -1);
-  });
+      it('sets global.stilrStylesheet to the rendered stylesheet', function () {
+        var expectedHeader =
+          fs.readFileSync(path.join(__dirname, 'general', 'expectedHeader.js'))
+            .toString()
+            .replace(/\n/g, '');
+        assert.deepEqual(global.stilrStylesheet,
+          '._1A3OE5{' +
+            '-webkit-box-align:center;' +
+            '-webkit-align-items:center;' +
+            '-ms-flex-align:center;' +
+            'align-items:center;}');
+      });
+
+    });
 
 });
+
+
